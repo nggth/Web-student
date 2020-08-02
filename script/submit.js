a44d1f0b0e00ef27988d4baadec0d4fac7473daf
ngthh <saccarozo.29@gmail.com>
2020-08-02 23:37:23 +0700

    login-submit

--- script/submit.js
+++ script/submit.js
@@ -1,3 +1,14 @@
+// Navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
 /*Submit From*/
 // Self-executing function
 (function() {
